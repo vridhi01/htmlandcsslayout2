64ded15018e3dafdc6c7c13459022d1ff06963b1
deepak <deepak@excellencetechnologies.in>
2020-10-22 15:42:17 +0530

    layout2

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Layout2</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="index.css">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+   </head>
+   <body>
+      <div class="navigation">
+         <div class="stylenavigation">
+            <li><a class="navigationlink1" href="#">Layout 3</a></li>
+            <li><a class="navigationlink2" href="#">About</a></li>
+            <li><a class="navigationlink3" href="#">Services</a></li>
+            <li><a class="navigationlink4" href="#">Contact</a></li>
+         </div>
+      </div>
+      <div class="middlediv">
+         <div class="innerdiv">
+            <div class="maindiv">
+               <h2 class="headingstyle"> Sample Heading  </h2>
+               <p class="paragraphstyle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's <span class="innerpara">standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley of type.</span>
+               </p>
+               <button type="button" class="buttonstyle"  > Call to action! </button>
+            </div>
+            <div class="firstrow">
+               <h2 class="hstyle"> Latest Features</h2>
+               <div class="firstdiv rpadding">
+                  <img src="div.png"  class="imgstyle "/>
+                  <div class="innerfirstdiv">
+                     <h2 class="boxheading">Feature Label</h2>
+                     <p class="boxparagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                     <button class="boxbutton1">Buy Now! </button>
+                     <button class="boxbutton2">More info</button>
+                  </div>
+               </div>
+               <div class="firstdiv rl1padding">
+                  <img src="div.png"  class="imgstyle "/>
+                  <div class="innerfirstdiv">
+                     <h2 class="boxheading">Feature Label</h2>
+                     <p class="boxparagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                     <button class="boxbutton1">Buy Now! </button>
+                     <button class="boxbutton2">More info</button>
+                  </div>
+               </div>
+               <div class="firstdiv rl2padding">
+                  <img src="div.png"  class="imgstyle "/>
+                  <div class="innerfirstdiv">
+                     <h2 class="boxheading">Feature Label</h2>
+                     <p class="boxparagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                     <button class="boxbutton1">Buy Now! </button>
+                     <button class="boxbutton2">More info</button>
+                  </div>
+               </div>
+               <div class="firstdiv lpadding">
+                  <img src="div.png"  class="imgstyle "/>
+                  <div class="innerfirstdiv">
+                     <h2 class="boxheading">Feature Label</h2>
+                     <p class="boxparagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                     <button class="boxbutton1">Buy Now! </button>
+                     <button class="boxbutton2">More info</button>
+                  </div>
+               </div>
+               </div>
+               <div class="footer">
+                  <div class="copyright">
+                     <p class="copyrighttext">  Copyright © Your Website 2014</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+        </body>
+    </html>
